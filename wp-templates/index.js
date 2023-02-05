533faa56d415560aa3ceabfdf4e3466e88b81935
Marissa Matthews <marissa.matthews@wpengine.com>
2023-02-05 11:23:54 -0500

    add test slug specific template

--- wp-templates/index.js
+++ wp-templates/index.js
@@ -1,13 +1,11 @@
-import category from './category';
-import tag from './tag';
-import frontPage from './front-page';
-import page from './page';
-import single from './single';
+import specificSlug from "./page-specific-slug";
+import food from "./food";
 
 export default {
   category,
   tag,
   'front-page': frontPage,
   page,
+  "page-specific-slug": specificSlug,
   single,
 };
